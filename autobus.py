d2f8598d371559796259e19a6f525589638b3387
elenaluk284 <elenaluk284@gmail.com>
2025-03-12 17:50:28 +0300

    add files

---
+++ autobus.py
@@ -0,0 +1,20 @@
+class Transport:
+    def __init__(self, name, max_speed, mileage):
+        self.name = name
+        self.max_speed = max_speed
+        self.mileage = mileage
+
+    def seating_capacity(self, capacity):
+        return f"Вместимость одного автобуса {self.name}: {capacity} пассажиров"
+
+
+class Autobus(Transport):
+    def seating_capacity(self, capacity=50):
+        return f"Вместимость одного автобуса {self.name}: {capacity} пассажиров"
+
+
+# Создаем объект Autobus
+autobus = Autobus("Renaul Logan", 180, 12)
+
+# Выводим информацию о вместимости
+print(autobus.seating_capacity())
